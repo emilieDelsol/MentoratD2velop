d96e704998c88cf0f916db3eb8f391e96a85d130
emilieDelsol <emiliedelsol1@gmail.com>
2021-01-31 23:22:17 +0100

    [+] exercice 3: algorithmes ajout dans un tableau :)

--- searchJson/scriptEx3.js
+++ searchJson/scriptEx3.js
@@ -1,10 +1,7 @@
 function addAtStartTable()
 {
     tableSelected();
-    let weightToAddStart = document.getElementById('weightId').value;
-    let labelToAddStart = document.getElementById('labelId').value;
-    let itemAdd=`{"weight": ${weightToAddStart}, "label":"${labelToAddStart}"}`;
-    let jsonAdd = JSON.parse(itemAdd);
+    let jsonAdd = createJsonObj();
     data.unshift(jsonAdd);
     console.log(data);
 
@@ -13,10 +10,7 @@ function addAtStartTable()
 function addAtEndTable()
 {
     tableSelected();
-    let weightToAddEnd = document.getElementById('weightId').value;
-    let labelToAddEnd = document.getElementById('labelId').value;
-    let item=`{"weight": ${weightToAddEnd}, "label":"${labelToAddEnd}"}`;
-    let json = JSON.parse(item);
+    let json = createJsonObj();
     data.push(json);
     console.log(data);
 }
@@ -24,11 +18,66 @@ function addAtEndTable()
 function addAtChooseId()
 {
     tableSelected();
-    let weightToAddAtId = document.getElementById('weightId').value;
-    let labelToAddAtId = document.getElementById('labelId').value;
-    let chooseId = document.getElementById('chooseId').value;
-    let item=`{"weight": ${weightToAddAtId}, "label":"${labelToAddAtId}"}`;
-    let json = JSON.parse(item);
+    let json = createJsonObj();
     data.splice(chooseId, 0, json);
     console.log(data);
+}
+
+function addElementAtStartTable()
+{
+    tableSelected();
+    let jsonAdd = createJsonObj();
+    let newEndId = data.length;
+    let i=0;
+
+    for(i=newEndId; i>=0; i--)
+    {
+        data[i+1]=data[i];
+    }
+    data[0] = jsonAdd;
+
+    console.log(data);
+}
+
+function addElementEndTable()
+{
+    tableSelected();
+    let jsonAdd = createJsonObj();
+    let endIndex = data.length-1;
+    let newEndId = data.length;
+    let i=0;
+
+    for(i=newEndId; i>=endIndex+1; i--)
+    {
+        data[i+1]= data[i];
+    }
+    data[endIndex+1] = jsonAdd;
+
+    console.log(data);
+
+}
+
+function addElementAfterChooseId()
+{
+    tableSelected();
+    let jsonAdd = createJsonObj();
+    let indexChoose = parseInt(document.getElementById('chooseId2').value)-1;
+    let initialLenght = data.length;
+    let i=0;
+    for(i = initialLenght; i >= indexChoose + 1; i--) 
+    {
+        data[i + 1] = data[i];
+     }
+     data[indexChoose + 1] = jsonAdd;
+     
+    console.log(data);
+}
+
+function createJsonObj()
+{
+    let weight = document.getElementById('weightId').value;
+    let label = document.getElementById('labelId').value;
+    let itemAdd=`{"weight": ${weight}, "label":"${label}"}`;
+    let jsonAdd = JSON.parse(itemAdd);
+    return jsonAdd;
 }
