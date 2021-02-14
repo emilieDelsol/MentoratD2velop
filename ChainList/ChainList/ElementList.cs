01c934ad719812bfc87fab6e534a5a1ccdfc9e3d
emilieDelsol <emiliedelsol1@gmail.com>
2021-02-14 23:23:05 +0100

    [+] add first exercice

--- ChainList/ChainList/ElementList.cs
+++ ChainList/ChainList/ElementList.cs
@@ -1,45 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace ChainList
 {
-	class ElementList
+	public class ElementList
 	{
-		private Int32 _weight;
-		private String _label;
-		private ElementList _next;
-		public ElementList(Int32 weight, String label, ElementList next)
+		public Int32 weight {get; private set; }
+
+		public String label { get; private set; }
+		
+		public ElementList next;
+
+		public ElementList(int weightEntry, String labelEntry, ElementList nextEntry)
 		{
-			_weight = weight;
-			_label = label;
-			_next = next;
+			weight = weightEntry;
+			label = labelEntry;
+			next = nextEntry;
 		}
 
-		public Int32 Getweight() 
-		{ 
-			return _weight; 
-		}
-		public String GetLabel() 
-		{ 
-			return _label; 
-		}
-		public void Setweight(Int32 weight)
-		{
-			_weight = weight;
-		}
-		public void SetLabel(String label)
-		{
-			_label = label;
-		}
-
-		public ElementList GetNext() 
-		{ 
-			return _next; 
-		}
-		public void SetNext(ElementList next)
-		{
-			_next = next;
-		}
 	}
 }
--- ChainList/ChainList/Program.cs
+++ ChainList/ChainList/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace ChainList
 {
@@ -7,6 +10,72 @@ namespace ChainList
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+
+			string path = SelectArray();
+
+			StreamReader streamReader = new StreamReader(path);
+			string json = streamReader.ReadToEnd();
+
+			List<WeightLabel> weighLabels = JsonConvert.DeserializeObject<List<WeightLabel>>(json);
+			
+			ChainList chainList = new ChainList();
+			foreach(WeightLabel element in weighLabels)
+			{
+				chainList.Add(element.weight, element.label);
+			}
+			
+			Console.WriteLine($"The chainList length is  {chainList.GetLength()}");
+			
+			searchLabel(chainList);
+		}
+
+		private static void searchLabel(ChainList chainList)
+		{
+			Console.WriteLine("Enter Label you search:");
+			string labelSearch = Console.ReadLine();
+			ElementList start = chainList.Start;
+			int number = 0;
+			if(start.label==labelSearch)
+			{
+				Console.WriteLine($"The first label = {labelSearch} is: number {number}:  {start.weight} {start.label} ");
+			}
+			else
+			{
+				ElementList previous = chainList.Start;
+				ElementList flag = chainList.Start.next;
+				while(flag.label!=labelSearch || flag.label!=null)
+				{
+					previous=flag;
+					flag = flag.next;
+					number++;
+				}
+				if(flag!=null)
+				{
+					Console.WriteLine($"The first label = {labelSearch} is: number {number}:  {start.weight} {start.label} ");
+				}
+
+			}
+
+		}
+
+		private static string SelectArray()
+		{
+			Console.WriteLine("select an array: \n\t1 for 1K, \n\t2 for 1M , \n\t3 for 50K");
+			string userChoice = Console.ReadLine();
+
+			if (userChoice == "1")
+			{
+				return @"D:\Documents\exercicesMentorat\arraySearch\arraySearch\1K.json";
+			}
+			else if (userChoice == "2")
+			{
+				return @"D:\Documents\exercicesMentorat\arraySearch\arraySearch\1M.json";
+			}
+			else if (userChoice == "3")
+			{
+				return @"D:\Documents\exercicesMentorat\arraySearch\arraySearch\50K.json";
+			}
+			throw new NotImplementedException();
 		}
 	}
 }
