e768de9bb28b3db3c7b38f1ab76b70bc67e94562
emilieDelsol <emiliedelsol1@gmail.com>
2021-02-14 17:19:33 +0100

    [+] add chainList exercice

---
+++ ChainList/ChainList/ChainList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainList
+{
+	class ChainList
+	{
+		private ElementList _start;
+
+		public Boolean IsEmpty() 
+		{ 
+			return _start == null; 
+		}
+		public ElementList GetStart() 
+		{ 
+			return _start; 
+		}
+		public void AddStart(int value, String label) 
+		{ 
+			ElementList oldFirst = _start; 
+			_start = new ElementList(value,label, oldFirst); 
+		}
+		
+		public int GetLength()
+		{
+			int length = 0;
+			ElementList flag = GetStart();
+			while (flag != null)
+			{
+				length++;
+				flag = flag.GetNext();
+			}
+			return length;
+		}
+		public Boolean Contain(int value)
+		{
+			Boolean isFound = false;
+			ElementList flag = GetStart();
+			while(! isFound && flag!=null)
+			{
+				if(flag.GetValue()==value)
+				{
+					isFound = true;
+				}
+				else
+				{
+					flag = flag.GetNext();
+				}
+			}
+			return isFound;
+		}
+
+		public void AddEnd(int value, String label)
+		{
+			if(IsEmpty())
+			{
+				_start = new ElementList(value, label);
+			}
+			else
+			{
+				ElementList latest = GetLatestElement();
+				latest.SetNext(new ElementList (value, label));
+			}
+		}
+
+		private ElementList GetLatestElement()
+		{
+			ElementList latest = _start;
+			while (latest.GetNext() != null)
+			{
+				latest = latest.GetNext();
+			}
+			return latest;
+		}
+
+
+		public void DeleteFirstOccurence(int value)
+		{
+			if(IsEmpty())
+			{
+				return;
+			}
+			if(_start.GetValue()==value)
+			{
+				_start = _start.GetNext();
+			}
+			else
+			{
+				ElementList previous = _start;
+				ElementList flag = _start.GetNext();
+				while (flag != null)
+				{
+					previous = flag;
+					flag = flag.GetNext();
+				}
+				if(flag!=null)
+				{
+					previous.SetNext(flag.GetNext());
+				}
+			}
+		}
+	}
+}
---
+++ ChainList/ChainList/ElementList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainList
+{
+	class ElementList
+	{
+		private int _value;
+		private String _label;
+		private ElementList _next;
+		public ElementList(int value, String label, ElementList next)
+		{
+			_value = value;
+			_label = label;
+			_next = next;
+		}
+		public ElementList(int value, String label)
+		{
+			_value = value;
+			_label = label;
+			_next = null;
+		}
+
+		public int GetValue() 
+		{ 
+			return _value; 
+		}
+		public String GetLabel() 
+		{ 
+			return _label; 
+		}
+		public void SetValue(int value)
+		{
+			_value = value;
+		}
+		public void SetLabel(String label)
+		{
+			_label = label;
+		}
+
+		public ElementList GetNext() 
+		{ 
+			return _next; 
+		}
+		public void SetNext(ElementList next)
+		{
+			_next = next;
+		}
+	}
+}
